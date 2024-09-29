9ca9407f43ad32ecc2b64144121ce64f5daec3a0
uillasnr <wiliasreis@hotmail.com>
2024-09-29 13:03:59 -0300

    feat add  componte input

---
+++ FrontEnd/mobile/components/input/index.tsx
@@ -0,0 +1,66 @@
+import { Colors } from "@/constants/Colors";
+import {
+  KeyboardTypeOptions,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { Controller } from "react-hook-form";
+
+interface InputProps {
+  name: string;
+  control: any;
+  placeholder?: string;
+  rules?: object;
+  error?: string;
+  keyboardType: KeyboardTypeOptions;
+}
+
+export function Input({
+  name,
+  control,
+  placeholder,
+  rules,
+  error,
+  keyboardType,
+}: InputProps) {
+  return (
+    <View style={styles.container}>
+        
+      <Controller
+        control={control}
+        name={name}
+        rules={rules}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder={placeholder}
+            onBlur={onBlur}
+            value={value}
+            onChangeText={onChange}
+            keyboardType={keyboardType}
+          />
+        )}
+      />
+
+      {error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 16,
+  },
+  input: {
+    height: 44,
+    backgroundColor: Colors.white,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+  },
+  errorText: {
+    color: "red",
+    marginTop: 4,
+  },
+});
