d60f03fb7de0dc10528f862a51f12c5fa4754509
uillasnr <wiliasreis@hotmail.com>
2024-10-02 20:40:15 -0300

    feat: add data

---
+++ FrontEnd/mobile/types/data.ts
@@ -0,0 +1,19 @@
+interface RefeicoesProps{
+  horario: string;
+  nome: string;
+  alimentos: string[];
+}
+
+export interface Data {
+  nome: string;
+  sexo: string;
+  idade: number;
+  altura: number;
+  peso: number;
+  objetivo: string; 
+  agua_diaria: string;
+  "refeições": RefeicoesProps[];
+  suplementos: string[];
+}
+
+  
