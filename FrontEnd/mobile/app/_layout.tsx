09cbc33d42e4a7743db5c4d89a9a0e7f497c346b
uillasnr <wiliasreis@hotmail.com>
2024-10-31 20:40:54 -0300

    feat: add routes

--- FrontEnd/mobile/app/_layout.tsx
+++ FrontEnd/mobile/app/_layout.tsx
@@ -1,17 +1,17 @@
 import { Stack } from "expo-router";
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default function RootLayout() {
   const queryClient = new QueryClient();
-  
+
   return (
     <QueryClientProvider client={queryClient}>
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="step/index" options={{ headerShown: false }} />
-      <Stack.Screen name="create/index" options={{ headerShown: false }} />
-      <Stack.Screen name="nutrition/index" options={{ headerShown: false }} />
-    </Stack>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="create/index" options={{ headerShown: false }} />
+        <Stack.Screen name="nutrition/index" options={{ headerShown: false }} />
+        <Stack.Screen name="step/index" options={{ headerShown: false }} />
+      </Stack>
     </QueryClientProvider>
   );
 }
